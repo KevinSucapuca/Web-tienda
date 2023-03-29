e2c29dce517ca17dd4bc585948d0fd0c4dbe6fed
KevinSucapuca <ksucapuca007@gmail.com>
2023-03-28 20:59:01 -0500

    add models and urls

--- Aplicaciones/Store/models.py
+++ Aplicaciones/Store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Marca (models.Model):
+    Nombre = models.CharField(max_length=35)
+    def __str__(self):
+        texto = "{0}"
+        return texto.format(self.Nombre)
+
+class Producto(models.Model):
+    NombreProducto=models.CharField(max_length=35)
+    Precio=models.DecimalField(max_digits=6, decimal_places=2)
+    Estado=models.CharField(max_length=35)
+    foto=models.URLField(max_length = 300)
+    Marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        texto = "{0} {1} {2} {3} {4}"
+        return texto.format(self.NombreProducto, self.Precio, self.Estado, self.foto, self.marca)
+
--- Aplicaciones/Store/views.py
+++ Aplicaciones/Store/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.db.models import Q
+from .models import Producto
+from django.core.paginator import Paginator
 # Create your views here.
+
+
+def home(request):
+
+    dataproducto = Producto.objects.all().order_by('-id')
+    paginator = Paginator(dataproducto, 8)
+    pagina = request.GET.get('page') or 1
+    dataproducto = paginator.get_page(pagina)
+    pagina_actual = int(pagina)
+    paginas = range(1, dataproducto.paginator.num_pages + 1)
+    
+    if 'buscar' in request.GET:
+        callsign = request.GET['buscar']
+        dataproducto = Producto.objects.filter(
+            Q(NombreProducto__icontains=callsign) |
+            Q(Marca__icontains=callsign))
+        paginator = Paginator(dataproducto, 8)
+        pagina = request.GET.get('page') or 1
+        dataproducto = paginator.get_page(pagina)
+        pagina_actual = int(pagina)
+        paginas = range(1, dataproducto.paginator.num_pages + 1)
+    
+    return render(request, 'galeria.html', {'dataproducto': dataproducto, 'paginas': paginas, 'pagina_actual': pagina_actual})
